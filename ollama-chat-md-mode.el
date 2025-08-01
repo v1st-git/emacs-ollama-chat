6c021794019fed9cd86cb19db614e8960ed2ed5b
v1st-git <68639553+v1st-git@users.noreply.github.com>
2025-08-01 10:11:41 +0300

    Update ollama-chat-md-mode.el
    minor code enhancements; comments updated

--- ollama-chat-md-mode.el
+++ ollama-chat-md-mode.el
@@ -1,4 +1,4 @@
-;;	$Id: ollama-chat-md-mode.el,v 1.9 2025/07/21 07:24:54 vst Exp vst $	
+;;	$Id: ollama-chat-md-mode.el,v 1.10 2025/08/01 07:08:40 vst Exp vst $	
 ;;; ollama-chat-md-mode.el --- chat with local LLM using Ollama API, markdown mode
 
 ;; Copyright (C) 2025- Vladimir Stavrov
@@ -6,7 +6,7 @@
 ;; Maintainer: https://github.com/v1st-git
 ;; Keywords: Emacs, Eliza, chat-bot, ollama, LLM
 
-;; This file is part of GNU Emacs.
+;; This file is not a part of GNU Emacs.
 
 ;; GNU Emacs is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -23,17 +23,18 @@
 
 ;;; Commentary:
 
-;; The single entry point `emacs-ollama-chat', implements a chat with LLM using ollama API.
+;; The single entry point 'emacs-ollama-chat', implements a chat with LLM using Ollama API.
 ;; It is successor of the classic ELIZA demonstration of pseudo-AI and doctor.el.
 
-
+;; Prerequisites: the following packages should be installed:
 (require 'json)
 (require 'cl-lib)
 (require 'url)
 (require 'markdown-mode)
 
+;; Customizable variables
 (defgroup ollama-chat-md-mode nil
-  "Ollama chat client for Emacs."
+  "Ollama chat client for Emacs using Markdown mode."
   :group 'ollama-chat-md-mode)
 
 (defcustom ollama-chat-md-mode:ollama-endpoint "http://localhost:11434/api/generate"
@@ -41,7 +42,7 @@
   :group 'ollama-chat-md-mode
   :type 'string)
 
-(defcustom ollama-chat-md-mode:model-name "gemma3:12b" ;; "phi4:latest"
+(defcustom ollama-chat-md-mode:model-name "gemma3:12b" ;; "phi4:latest" etc...
   "Ollama model."
   :group 'ollama-chat-md-mode
   :type 'string)
@@ -96,6 +97,8 @@ and prints the answer, received from Ollama API"
 		     "<!--" "Here you can interact with LLM model by Ollama API."
 			  "Customize LLM model name etc in customization group"
 			  "'ollama-chat-md-mode'"
+			  (concat "Model name:" ollama-chat-md-mode:model-name)
+
 			  "Each time you are finished talking, type RET twice ." "-->"
 			  ollama-chat-md-mode:human-keystring))
   (insert "\n")
@@ -127,16 +130,14 @@ and prints the answer, received from Ollama API"
     ))
 
 (defun ollama-chat-readin ()
-  "Read a sentence.  Return it as a string."
+  "Read a sentence from the last human keystring to the end in the current buffer.
+   Return it as a string after removing of surrounding blanks."
   (goto-char (point-max))
   (search-backward ollama-chat-md-mode:human-keystring) ; find latest human keystring
   (search-forward ollama-chat-md-mode:human-keystring) ; set point after keystring
-  (while (and (not (eobp))         ; Ensure we don't go past end of buffer
-              (looking-at "^[ \t\n]*$")) ; Check if line is only whitespace
-    (forward-line 1))             ; Move to the next line
-
   (let ((sentence
-	 (buffer-substring-no-properties (point) (goto-char (point-max)))))
+	 (string-trim (buffer-substring-no-properties (point) (goto-char (point-max))))
+	 ))
     sentence
     ))
 
@@ -173,7 +174,7 @@ and prints the answer, received from Ollama API"
 		(json-read-from-string (format "%s" json_string))))
       (error
        (concat
-	(format "\nollama-chat-get-json-response-from-string error occurs: %s\n"
+	(format "\nollama-chat-get-json-tag-from-string error occurs: %s\n"
   		(error-message-string err))
 	(format "in the string:\n%s\n" json_string))
        )
@@ -244,8 +245,6 @@ and prints the answer, received from Ollama API"
 	 )
     llm_answer_string
     ))
-
-
 
 (provide 'ollama-chat-md-mode)
 
